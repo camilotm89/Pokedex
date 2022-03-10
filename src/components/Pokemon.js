756dcc176d4f8f249147a119cdb9d297706e06df
camilo <camilotm89@gmail.com>
2022-03-10 10:51:43 -0500

    add bootstrap

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,28 +3,31 @@ import React from 'react';
 const Pokemon = (props) => {
     const {pokemon} = props;
     return(
-        <div className='pokemon-card'>
-            <div className='pokemon-img'>
-                <img 
-                    src= {pokemon.sprites.front_default} 
-                    alt= {pokemon.name} 
-                />
-            </div>
-            <div>
-                <div>
-                    <h3>{pokemon.name}</h3>
-                    <div>#{pokemon.id}</div>
+        <div className='container'>
+            <div className='shadow p-3 mb-5 bg-body rounded'>
+                <div className='pokemon-img-container'>
+                    <img 
+                        src= {pokemon.sprites.front_default} 
+                        alt= {pokemon.name} 
+                        className='card-img-top pokemon-img'
+                    />
                 </div>
-                <div>
-                    <div>
-                        {pokemon.types.map((type, idx) => {
-                            return (
-                            <div key={idx}>{type.name}</div>
-                            );
-                            
-                        })}
+                <div className='card-body'>
+                    <div className='card-top'>
+                        <h3>{pokemon.name}</h3>
+                        <div>#{pokemon.id}</div>
                     </div>
-                    <div> ❤️ </div>
+                    <div className='card-bottom'>
+                        <div className='pokemon-type'>
+                            {pokemon.types.map((type, idx) => {
+                                return (
+                                <div key={idx}>{type.type.name}</div>
+                                );
+                                
+                            })}
+                        </div>
+                        <div className='pokemon-favorite'> ❤️ </div>
+                    </div>
                 </div>
             </div>
         </div>
